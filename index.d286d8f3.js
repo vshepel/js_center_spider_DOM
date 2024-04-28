a096b0c3b19e84f7e56dd2981a16c6aed1db98d2
Volodymyr Shepel <web.shepel@gmail.com>
2024-04-28 18:17:50 +0300

    make build

---
+++ index.d286d8f3.js
@@ -0,0 +1,2 @@
+var e,t,c,l=document.querySelector(".wall");e=document.querySelector(".spider"),t=(l.clientHeight-e.offsetHeight)/2,c=(l.clientWidth-e.offsetWidth)/2,e.style.top="".concat(t,"px"),e.style.left="".concat(c,"px");
+//# sourceMappingURL=index.d286d8f3.js.map
